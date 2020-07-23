0dddd1f902716929e89f5a7416f5c58b0dd6548d
ingrid <ingrid.ortiz2016@uteq.edu.ec>
2020-07-23 17:51:50 -0500

    Segundo commit

---
+++ app/src/main/java/com/example/evaluacioninformacionciudades/Retrofit/PaisesNomRetrofit.java
@@ -0,0 +1,15 @@
+package com.example.evaluacioninformacionciudades.Retrofit;
+
+import com.example.evaluacioninformacionciudades.Model.Model;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+
+public interface PaisesNomRetrofit {
+    @GET("rest/v2/all")
+    Call<List<Model>> getPost();
+}
