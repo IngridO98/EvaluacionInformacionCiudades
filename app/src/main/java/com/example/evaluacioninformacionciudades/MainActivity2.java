ce3b7046a72e647c3856a146ca69aceb37137583
ingrid <ingrid.ortiz2016@uteq.edu.ec>
2020-07-23 23:07:04 -0500

    Noveno commit

--- app/src/main/java/com/example/evaluacioninformacionciudades/MainActivity2.java
+++ app/src/main/java/com/example/evaluacioninformacionciudades/MainActivity2.java
@@ -1,14 +1,64 @@
 package com.example.evaluacioninformacionciudades;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity2 extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity2 extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    FragmentManager fragmenManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout=findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle =new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        fragmenManager=getSupportFragmentManager();
+        fragmentTransaction=fragmenManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_fragment, new GeneralFragment());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.General){
+
+        }
+
+        if(item.getItemId()==R.id.Lenguaje){
+
+        }
+
+        if(item.getItemId()==R.id.Bandera){
+
+        }
+        return false;
     }
 }
